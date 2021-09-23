3436c72224cdcb23077ddfd8b31e86ed75352318
ValleAD <angel.valle1208@gmail.com>
2021-09-23 13:58:38 -0600

    Update

---
+++ app/src/main/java/com/itca/eval_practica_ii/registerNotes.java
@@ -0,0 +1,14 @@
+package com.itca.eval_practica_ii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registerNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_notes);
+    }
+}
---
+++ app/src/main/java/com/itca/eval_practica_ii/update.java
@@ -0,0 +1,14 @@
+package com.itca.eval_practica_ii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
