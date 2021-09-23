8a1c922eb6196e026b376467b304f4333e28b69d
Alvitronex <82683379+Alvitronex@users.noreply.github.com>
2021-09-23 14:22:39 -0600

    Update and RegisterNotes class

--- app/src/main/java/com/itca/eval_practica_ii/registerNotes.java
+++ app/src/main/java/com/itca/eval_practica_ii/registerNotes.java
@@ -2,13 +2,59 @@ package com.itca.eval_practica_ii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class registerNotes extends AppCompatActivity {
+    public ListView list;
+    public TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_notes);
+        list = findViewById(R.id.notasList);
+        text = findViewById(R.id.textView);
+
+        ArrayList <String> valor = new ArrayList<>();
+
+
+        ConexionSQLite conexion = new ConexionSQLite(this);
+        SQLiteDatabase bd = conexion.getWritableDatabase();
+
+        Cursor fila = bd.rawQuery("select titulo from tb_Notas", null);
+        if(fila.moveToFirst()) {
+            do {
+                valor.add(fila.getString(0));
+            }  while (fila.moveToNext());
+        }
+        bd.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, valor);
+        list.setAdapter(adapter);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(view.getContext(), update.class);
+                //String t = valor.get(position);
+                String t = (String) list.getItemAtPosition(position);
+
+                intent.putExtra("valorTitulo", t);
+                startActivity(intent);
+            }
+        });
+
     }
-}
--- app/src/main/java/com/itca/eval_practica_ii/update.java
+++ app/src/main/java/com/itca/eval_practica_ii/update.java
@@ -2,13 +2,95 @@ package com.itca.eval_practica_ii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class update extends AppCompatActivity {
+    /*Bundle bundle = getIntent().getExtras();
+    String t = (String) bundle.getString("titulo");*/
+    public String x;
+    public EditText v;
+    public EditText titulo, desc, autor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        titulo = findViewById(R.id.txtTitle);
+        desc = findViewById(R.id.txtDescription);
+        autor = findViewById(R.id.txtAutor);
+
+        Bundle bundle = new Bundle();
+
+        String dato = getIntent().getStringExtra("valorTitulo");
+
+        v = findViewById(R.id.txtTitle);
+        v.setText(dato);
+
+        x = v.getText().toString();
+
+
+        try {
+
+            ConexionSQLite conexion = new ConexionSQLite(this);
+            SQLiteDatabase bd = conexion.getWritableDatabase();
+
+            Cursor fila = bd.rawQuery("select descripcion, autor from tb_Notas where titulo = '" + x + "'"  , null);
+            if(fila.moveToFirst()) {
+                desc.setText(fila.getString(0));
+                autor.setText(fila.getString(1));
+            }
+
+
+        } catch (Exception e) {
+            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
+
+
+    public void update(View view) {
+        ConexionSQLite conexion = new ConexionSQLite(this);
+        SQLiteDatabase bd = conexion.getWritableDatabase();
+        String t = titulo.getText().toString();
+        String d = desc.getText().toString();
+        String a = autor.getText().toString();
+
+
+        ContentValues registro = new ContentValues();
+        registro.put("titulo", t);
+        registro.put("descripcion", d);
+        registro.put("autor", a);
+
+        int cant = bd.update("tb_Notas", registro, "titulo = '" + x + "'", null);
+        bd.close();
+        if (cant == 1) {
+            Toast.makeText(this, "Nota Modificada", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Nota NO Modificada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void eliminarNota(View view) {
+        ConexionSQLite conexion = new ConexionSQLite(this);
+        SQLiteDatabase bd = conexion.getWritableDatabase();
+        String t = titulo.getText().toString();
+        int cant = bd.delete("tb_Notas", "titulo = '" + x + "'", null);
+        bd.close();
+        titulo.setText("");
+        desc.setText("");
+        autor.setText("");
+
+        Toast.makeText(this, "Nota Eliminada", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
